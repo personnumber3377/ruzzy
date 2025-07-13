000e1767bcbfceb816d0bf06d6f11b784c51f381
personnumber3377 <sontapaa.jokulainen@gmail.com>
2025-07-13 22:24:46 +0300

    Blah

---
+++ ext/cruzzy/get_stuff.sh
@@ -0,0 +1,4 @@
+ruby -rrbconfig -e 'puts RbConfig::CONFIG["rubyhdrdir"]'
+ruby -rrbconfig -e 'puts RbConfig::CONFIG["rubyarchhdrdir"]'
+# /var/lib/gems/3.2.0/gems/ruzzy-0.7.0/lib/cruzzy/cruzzy.so
+#
